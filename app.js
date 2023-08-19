414503ac542575f3a087223106cb65ae83f5edf3
OumarOuattara <greatoumar@hotmail.de>
2023-08-19 06:49:03 +0200

    Added coin game

---
+++ app.js
@@ -0,0 +1,28 @@
+function isTouching(a, b) {
+  const aRect = a.getBoundingClientRect();
+  const bRect = b.getBoundingClientRect();
+
+  return !(
+    aRect.top + aRect.height < bRect.top ||
+    aRect.top > bRect.top + bRect.height ||
+    aRect.left + aRect.width < bRect.left ||
+    aRect.left > bRect.left + bRect.width
+  );
+}
+
+const avatar = document.querySelector("#player");
+
+window.addEventListener("keyup", function (e) {
+  if (e.key === "ArrowDown" || e.key === "Down") {
+    const currTop = extractPos(avatar.style.top);
+    avatar.style.top = `${currTop + 50}px`;
+  } else if (e.key === "ArrowUp" || e.key === "Up") {
+    const currTop = extractPos(avatar.style.top);
+    avatar.style.top = `${currTop - 50}px`;
+  }
+});
+
+const extractPos = (pos) => {
+  if (!pos) return 100;
+  parseInt(pos.slice(0, -2));
+};
